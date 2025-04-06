18a9e35feeb44adc04ef6fb0600e4d34bee9eded
liangchow <liangchern@gmail.com>
2025-04-06 22:12:11 +0800

    Update css header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import NorMap from './assets/us-map.svg?react'
-
+import Header from '../components/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <NorMap className="normap" />
     </>
   )
