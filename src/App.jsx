64414cca4ce0ef6efbb3ac902735a059c041e11a
liangchow <liangchern@gmail.com>
2025-04-13 22:07:45 +0800

    Update useState
    Update useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'
 import Main from '../components/Main'
 import SideBar from '../components/SideBar'
 import Footer from '../components/Footer'
 
 function App() {
 
+  const [showModal, setShowModal] = useState(false)
+
+  function handleToggleModal(){
+    setShowModal(!showModal)
+  }
+
   return (
     <>
       <Main />
-      <SideBar />
+      {showModal && (<SideBar />)}
     </>
   )
 }
