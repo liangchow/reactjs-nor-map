65100168a8724434cd6953dfe4878e0541230c56
liangchow <liangchern@gmail.com>
2025-04-25 16:54:33 -0700

    Create index.js
    Initial draft

---
+++ src/utils/index.js
@@ -0,0 +1,110 @@
+export const stateInfo = {
+
+    0: { //AL
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    1: { //AK
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    2: { //AZ
+        status : "active",
+        order : 8,
+        background: "House Bill 2081 /Senate Bill 1042 was signed into law by Governor Katie Hobbs on March 29, 2024. The bill, sponsored by Republican Representative Laurin Hendrix and Sen. TJ Shope, was introduced in December of 2023.",
+    },
+
+    3: { //AR
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+    
+    4: { //CA
+        status : "active",
+        order : 5,
+        background: "AB351 was the third CA bill aimed at legalizing human composting, following AB 2592 in 2020 and AB501 in 2021. Governor Newsom signed the natural organic reduction bill September 18, 2022.",
+    },
+
+    5: { //CO
+        status : "active",
+        order : 5,
+        background: "In early 2020, Recompose partnered with Colorado State Representative Brianna Titone to bring House Bill 20-1060 forward. After overwhelming support, the bill was postponed indefinitely due to budget concerns related to the COVID-19 pandemic. However, that wasn’t the end for human composting in Colorado. Representative Titone resurrected the bill during the next legislative session with her colleague, Representative Matt Soper. It passed with bipartisan support and was signed into law by Governor Jared Polis on May 10, 2021.",
+    },
+
+    6: { //CT
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    7: { //DE
+        status : "active",
+        order : 10,
+        background: "The Delaware House followed by the Senate approved Bill 162 allowing the composting of human bodies as an alternative to burial or cremation. After nearly two months since the vote passed in the senate, the legislation was signed into law by the Governor John Carney May 16, 2024.",
+    },
+
+    8: { //FL
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    9: { //GA
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    9: { //HA
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    10: { //ID
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    11: { //IL
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    12: { //IN
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    13: { //IA
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    14: { //KS
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    15: { //KY
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+
+    16: { //LA
+        status : "inactive",
+        order : null,
+        background: "",
+    },
+}
