b679a78603e25fce30e593a682888749ab829459
liangchow <liangchern@gmail.com>
2025-05-17 13:30:29 -0700

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,11 +8,12 @@ export default function Footer(props) {
     <footer>
       <div id="footer-top">
          <h1>NOR PROJECT</h1>
-         <h2>{stateDisplay.name} {stateDisplay.id}</h2>
+         <h2>{stateDisplay.name}</h2>
       </div>
-      <div id="footer-bottom">     
+      <div id="footer-bottom">
+          <small>Created by </small>     
           <a target="_blank" href="https://www.linkedin.com/in/lcchow">
-            <p>Created by @lcchow</p>
+            <p>@lcchow</p>
             <i className="fa-brands fa-linkedin-in"></i>
           </a>
       </div>
